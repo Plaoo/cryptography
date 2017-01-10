b198352acb220ce121bfa933bf01280a80e8a970
Plaoo <paolo.monni345@gmail.com>
2017-01-10 23:59:18 +0100

    add atbash algorithm

---
+++ atbash/atbash.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  atbash.py
+#  
+#  Copyright 2016 Paolo "Plaoo" Monni 
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#
+import sys
+from string import ascii_uppercase
+from string import ascii_lowercase
+from string import ascii_letters 
+  
+def how_use():
+	
+	
+	print("example: atbash.py hello world /n Result: svool dliow")
+
+def atbash(txt):
+	dest = []
+	ascii_loweruni = unicode(ascii_lowercase)
+	ascii_upperuni = unicode(ascii_uppercase)
+	ascii_letuni = unicode(ascii_letters)
+	#reverse alphabet
+	ascii_lowerunirev = unicode(ascii_lowercase[::-1])
+	ascii_upperunirev = unicode(ascii_uppercase[::-1])
+	ascii_letunirev = unicode(ascii_letters[::-1])
+	
+	
+	for mov in txt:
+		#not a ascii character
+		if mov not in ascii_letuni:
+			dest.append(mov)
+			continue
+		if mov in ascii_letters:
+			if mov in ascii_loweruni:
+				#lower case
+				alphab = ascii_lowerunirev
+				x = alphab.index(mov)
+				dest.append(ascii_loweruni[x])
+				
+			elif mov in ascii_upperuni:
+				#upper case
+				alphab = ascii_upperunirev
+				#x position in reverse alphabet
+				x = alphab.index(mov)
+				#position x in normal alphabet
+				dest.append(ascii_upperuni[x])
+
+	dest = ''.join(dest)
+	return dest
+	
+def main(args):
+	if(len(sys.argv)<=1):
+		how_use()
+	elif(len(sys.argv)>1):
+		print("Result: " + atbash(' '.join(sys.argv[1:])))
+		
+
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
